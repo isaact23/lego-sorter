edc70a1a33ca2b1f1348d27a6fae83617d5bb7a8
redshedjed <athompson.vt@gmail.com>
2026-01-10 18:24:55 -0500

    Updated app.js look/feel, added category breakdown

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -71,6 +71,7 @@ function App () {
   }
 
   return (
+    /*
     <div className='App w3-theme-light'>
       <div className='w3-bar w3-theme-d1'>
         <div className='w3-bar-item'>
@@ -85,6 +86,62 @@ function App () {
       </div>
       {getPage()}
     </div>
+    */ 
+    <div className='App w3-theme-light'>
+      <div className="option-card-row">
+        {/* Card 1: two dropdowns */}
+        <OptionCard iconSrc="/icons/mag_glass.png">
+          <select className="w3-select w3-border option-select">
+            <option>First dropdown option</option>
+          </select>
+
+          <select className="w3-select w3-border option-select">
+            <option>Second dropdown option</option>
+          </select>
+        </OptionCard>
+
+        {/* Card 2: text input */}
+        <OptionCard iconSrc="/icons/typewriter.png">
+          <input
+            className="w3-input w3-border"
+            placeholder="Enter text"
+          />
+        </OptionCard>
+
+        {/* Card 3: action button */}
+        <OptionCard
+          iconSrc="/icons/cam.png"
+          onClick={() => console.log("Find Brick")}
+        >
+          <strong>Find Brick</strong>
+        </OptionCard>
+      </div>
+      {getPage()}
+    </div>
   )
 }
+function OptionCard({ iconSrc, iconAlt = "", children, onClick }) {
+  return (
+    <div
+      className={`option-card ${onClick ? "option-card-clickable" : ""}`}
+      onClick={onClick}
+      role={onClick ? "button" : undefined}
+      tabIndex={onClick ? 0 : undefined}
+      onKeyDown={onClick ? e => e.key === "Enter" && onClick() : undefined}
+    >
+      <div className="option-card-icon">
+        <img
+          src={iconSrc}
+          alt={iconAlt}
+          className="option-card-icon-img"
+        />
+      </div>
+
+      <div className="option-card-content">
+        {children}
+      </div>
+    </div>
+  )
+}
+
 export default App
