2decaba3b7767d08ae44fbf9a3e8d542c18ce2b0
Isaac Thompson <shufflesninja@gmail.com>
2026-01-10 18:52:14 -0500

    Find brick button works

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -3,12 +3,15 @@ import Camera from '../Camera/Camera'
 import Select from '../Select/Select'
 import Table from '../Table/Table'
 import BucketEditor from '../BucketEditor/BucketEditor'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+import axios from 'axios'
 
 const CAMERA_PAGE = 0
 const SELECT_PAGE = 1
 const TABLE_PAGE = 2
 const EDIT_BUCKET_PAGE = 3
+
+const API_ENDPOINT = 'https://api.brickognize.com/predict/'
 
 function App () {
   // App state variables (React hooks)
@@ -16,6 +19,8 @@ function App () {
   const [brickList, setBrickList] = useState([])
   const [brick, setBrick] = useState(null)
   const [bucketId, setBucketId] = useState(0)
+  const [waiting, setWaiting] = useState(false)
+  const pictureInputRef = useRef(null)
 
   // Callback for the Camera component once Lego bricks are identified
   function brickCallback (bricks) {
@@ -70,9 +75,70 @@ function App () {
     if (page === EDIT_BUCKET_PAGE) return <BucketEditor bucketId={bucketId} />
   }
 
+  //
+  async function identify (base64Data) {
+    setWaiting(true)
+
+    const base64 = await fetch(base64Data)
+    const blob = await base64.blob()
+
+    const formData = new FormData()
+    formData.append('query_image', blob, 'image.jpg')
+
+    axios
+      .post(API_ENDPOINT, formData, {
+        headers: { Accept: 'application/json' }
+      })
+      .then(res => {
+        const legoList = res.data.items
+        if (legoList.length === 0) {
+          alert('No pieces identified, try again?')
+        } else {
+          brickCallback(legoList)
+        }
+        setWaiting(false)
+      })
+      .catch(err => {
+        console.error(err)
+        alert('Something went wrong.')
+        setWaiting(false)
+      })
+  }
+
+  // Handle when Take Picture button is pressed.
+  function takePicture () {
+    if (waiting) return
+
+    // Call the <input> element to fetch the file.
+    // That will call the handleFileChange callback once a file is chosen.
+    pictureInputRef.current.click()
+  }
+
+  // Handle when an image is taken.
+  function handleFileChange (event) {
+    const file = event.target.files[0]
+    if (!file) return
+
+    const reader = new FileReader()
+    reader.onloadend = () => {
+      identify(reader.result)
+    }
+    reader.readAsDataURL(file)
+  }
+
   return (
     <div className='App w3-theme-light'>
       <div className='option-card-row w3-container w3-padding'>
+        <input
+          id='cameraInput'
+          type='file'
+          accept='image/*'
+          capture='environment'
+          hidden
+          onChange={handleFileChange}
+          ref={pictureInputRef}
+        />
+
         {/* Card 1: two dropdowns */}
         <OptionCard iconSrc='/icons/mag_glass.png'>
           <select className='w3-select w3-border option-select'>
@@ -90,10 +156,7 @@ function App () {
         </OptionCard>
 
         {/* Card 3: action button */}
-        <OptionCard
-          iconSrc='/icons/cam.png'
-          onClick={() => console.log('Find Brick')}
-        >
+        <OptionCard iconSrc='/icons/cam.png' onClick={() => takePicture()}>
           <strong>Find Brick</strong>
         </OptionCard>
       </div>
